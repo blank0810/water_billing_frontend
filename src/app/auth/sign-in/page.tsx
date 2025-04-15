27692be4043dc4becc4019e21dd5a77e38190de8
blank <azucenaehnand@gmail.com>
2025-04-15 22:16:31 +0800

    analytics

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -20,6 +20,7 @@ import Link from 'next/link';
 import { MdOutlineRemoveRedEye } from 'react-icons/md';
 import { RiEyeCloseLine } from 'react-icons/ri';
 import { useRouter } from 'next/navigation';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function SignIn() {
   const router = useRouter();
@@ -36,122 +37,130 @@ export default function SignIn() {
   };
 
   return (
-    <DefaultAuthLayout illustrationBackground={'/img/auth/auth.jpg'}>
-      <Flex
-        maxW={{ base: '100%', md: 'max-content' }}
-        w="90%"
-        mx={{ base: 'auto', lg: '0px' }}
-        me="auto"
-        h="90%"
-        alignItems="center"
-        justifyContent="center"
-        flexDirection="column"
-      >
-        <Box textAlign="center" mb="20px">
-          <Image
-            src="/img/auth/logo.png"
-            alt="Logo"
-            mb="20px"
-            width="120px"
-            height="auto"
-            mx="auto"
-          />
-          <Heading color={textColor} fontSize="36px" mb="10px" textAlign="center">
-            ADMINISTRATOR LOGIN
-          </Heading>
-          <Text
-            mb="10px"
-            ms="4px"
-            color={textColorSecondary}
-            fontWeight="400"
-            fontSize="md"
-            textAlign="center"
-          >
-            Make sure to enter your registered username and password.!
-          </Text>
-        </Box>
-        <FormControl>
-          <FormLabel
-            display="flex"
-            ms="4px"
-            fontSize="sm"
-            fontWeight="500"
-            color={textColor}
-            mb="8px"
-          >
-            Username<Text color={brandStars}>*</Text>
-          </FormLabel>
-          <Input
-            isRequired={true}
-            variant="auth"
-            fontSize="sm"
-            ms={{ base: '0px', md: '0px' }}
-            type="email"
-            placeholder="Enter username here."
-            mb="24px"
-            fontWeight="500"
-            size="lg"
-          />
-          <FormLabel
-            ms="4px"
-            fontSize="sm"
-            fontWeight="500"
-            color={textColor}
-            display="flex"
-          >
-            Password<Text color={brandStars}>*</Text>
-          </FormLabel>
-          <InputGroup size="md">
+    <>
+      <Analytics />
+      <DefaultAuthLayout illustrationBackground={'/img/auth/auth.jpg'}>
+        <Flex
+          maxW={{ base: '100%', md: 'max-content' }}
+          w="90%"
+          mx={{ base: 'auto', lg: '0px' }}
+          me="auto"
+          h="90%"
+          alignItems="center"
+          justifyContent="center"
+          flexDirection="column"
+        >
+          <Box textAlign="center" mb="20px">
+            <Image
+              src="/img/auth/logo.png"
+              alt="Logo"
+              mb="20px"
+              width="120px"
+              height="auto"
+              mx="auto"
+            />
+            <Heading
+              color={textColor}
+              fontSize="36px"
+              mb="10px"
+              textAlign="center"
+            >
+              ADMINISTRATOR LOGIN
+            </Heading>
+            <Text
+              mb="10px"
+              ms="4px"
+              color={textColorSecondary}
+              fontWeight="400"
+              fontSize="md"
+              textAlign="center"
+            >
+              Make sure to enter your registered username and password.!
+            </Text>
+          </Box>
+          <FormControl>
+            <FormLabel
+              display="flex"
+              ms="4px"
+              fontSize="sm"
+              fontWeight="500"
+              color={textColor}
+              mb="8px"
+            >
+              Username<Text color={brandStars}>*</Text>
+            </FormLabel>
             <Input
               isRequired={true}
-              fontSize="sm"
-              placeholder="Min. 8 characters"
-              mb="24px"
-              size="lg"
-              type={show ? 'text' : 'password'}
               variant="auth"
+              fontSize="sm"
+              ms={{ base: '0px', md: '0px' }}
+              type="email"
+              placeholder="Enter username here."
+              mb="24px"
+              fontWeight="500"
+              size="lg"
             />
-            <InputRightElement display="flex" alignItems="center" mt="4px">
-              <Icon
-                color={textColorSecondary}
-                _hover={{ cursor: 'pointer' }}
-                as={show ? RiEyeCloseLine : MdOutlineRemoveRedEye}
-                onClick={handleClick}
-              />
-            </InputRightElement>
-          </InputGroup>
-          <Flex justifyContent="space-between" align="center" mb="24px">
-            <Link href="/auth/forgot-password">
-              <Text
-                color={textColorBrand}
+            <FormLabel
+              ms="4px"
+              fontSize="sm"
+              fontWeight="500"
+              color={textColor}
+              display="flex"
+            >
+              Password<Text color={brandStars}>*</Text>
+            </FormLabel>
+            <InputGroup size="md">
+              <Input
+                isRequired={true}
                 fontSize="sm"
-                w="124px"
-                fontWeight="500"
-              >
-                Forgot password?
-              </Text>
-            </Link>
-          </Flex>
-          <Button
-            fontSize="sm"
-            variant="brand"
-            fontWeight="500"
-            w="100%"
-            h="50"
-            mb="24px"
-            onClick={handleSignIn}
-          >
-            Sign In
-          </Button>
-        </FormControl>
-        <Flex
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="start"
-          maxW="100%"
-          mt="0px"
-        ></Flex>
-      </Flex>
-    </DefaultAuthLayout>
+                placeholder="Min. 8 characters"
+                mb="24px"
+                size="lg"
+                type={show ? 'text' : 'password'}
+                variant="auth"
+              />
+              <InputRightElement display="flex" alignItems="center" mt="4px">
+                <Icon
+                  color={textColorSecondary}
+                  _hover={{ cursor: 'pointer' }}
+                  as={show ? RiEyeCloseLine : MdOutlineRemoveRedEye}
+                  onClick={handleClick}
+                />
+              </InputRightElement>
+            </InputGroup>
+            <Flex justifyContent="space-between" align="center" mb="24px">
+              <Link href="/auth/forgot-password">
+                <Text
+                  color={textColorBrand}
+                  fontSize="sm"
+                  w="124px"
+                  fontWeight="500"
+                >
+                  Forgot password?
+                </Text>
+              </Link>
+            </Flex>
+            <Button
+              fontSize="sm"
+              variant="brand"
+              fontWeight="500"
+              w="100%"
+              h="50"
+              mb="24px"
+              onClick={handleSignIn}
+            >
+              Sign In
+            </Button>
+          </FormControl>
+          <Flex
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="start"
+            maxW="100%"
+            mt="0px"
+          ></Flex>
+        </Flex>
+      </DefaultAuthLayout>
+    </>
   );
 }
