5ef37b368356ec138cb30714335a26b0a7d94ec6
KlentDev <klentmicko@gmail.com>
2025-04-17 18:30:40 +0800

    Added admin dashboard some basic components, graphs, charts, and random features

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -1,4 +1,5 @@
 import { extendTheme, HTMLChakraProps, ThemingProps } from '@chakra-ui/react';
+import { CardComponent } from './additions/card/card';
 import { buttonStyles } from './components/button';
 import { badgeStyles } from './components/badge';
 import { inputStyles } from './components/input';
@@ -21,6 +22,7 @@ export default extendTheme(
 	inputStyles, // input styles
 	textareaStyles, // textarea styles
 	switchStyles, // switch styles
+	CardComponent // card component
 );
 
 export interface CustomCardProps extends HTMLChakraProps<'div'>, ThemingProps {}
